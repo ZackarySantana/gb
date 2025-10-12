764bcb57a2b631e73b58a721b714b2be1dde4cbe
ZackarySantana <64446617+ZackarySantana@users.noreply.github.com>
2025-10-12 13:03:14 -0400

    feat: export function be more async

--- export.go
+++ export.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"slices"
 	"time"
 
 	"github.com/urfave/cli/v3"
@@ -17,14 +18,27 @@ type ManifestCommit struct {
 	NoteRefs []string `json:"note_refs"`
 }
 
+type ManifestBenchmark struct {
+	Name    string   `json:"name"`
+	Commits []string `json:"commits"`
+}
+
 type Manifest struct {
 	// Metadata information
-	GeneratedAt  time.Time `json:"generated_at"`
-	LatestCommit string    `json:"latest_commit"`
 	Module       string    `json:"module"`
+	LatestCommit string    `json:"latest_commit"`
+	GeneratedAt  time.Time `json:"generated_at"`
+
+	// Benchmarks are all benchmarks found in the notes.
+	Benchmarks []ManifestBenchmark `json:"benchmarks"`
 
 	// Commits are sorted by newest to oldest.
 	Commits []ManifestCommit `json:"commits"`
+}
+
+type exportPayload struct {
+	output string
+	data   any
 }
 
 func (cmd *cmd) Export() *cli.Command {
@@ -61,7 +75,7 @@ func (cmd *cmd) Export() *cli.Command {
 				return nil
 			}
 
-			cmd.logger.InfoContext(ctx, "sync start", "range", rangeSpec)
+			cmd.logger.InfoContext(ctx, "export start", "range", rangeSpec)
 
 			refs, err := listAllNotesRefs(ctx)
 			if err != nil {
@@ -75,87 +89,127 @@ func (cmd *cmd) Export() *cli.Command {
 			manifest := &Manifest{
 				GeneratedAt:  time.Now().UTC(),
 				LatestCommit: commits[0],
-				Module:       "TODO-MODULE-NAME",
 			}
+			writeTasks := make(chan exportPayload, 10)
 
-			type noteInfo struct {
-				fileName string
-				value    []byte
-			}
-			ch := make(chan noteInfo, 10)
 			group, errCtx := errgroup.WithContext(ctx)
-
 			group.Go(func() error {
-				for {
+				return exportNote(errCtx, writeTasks)
+			})
+
+			emptyCommits := 0
+			benchmarks := map[string]*ManifestBenchmark{}
+			for _, commit := range commits {
+				var noteRefs []string
+				for _, ref := range refs {
+					value, err := gitNotesShow(errCtx, ref, commit)
+					if err != nil {
+						continue // This commit doesn't have this note ref, skip it.
+					}
+					var note Note
+					if err := json.Unmarshal(value, &note); err != nil {
+						cmd.logger.WarnContext(errCtx, "unknown note ref", "err", err, "commit", commit, "ref", ref)
+						continue
+					}
+					noteRefs = append(noteRefs, ref)
 					select {
 					case <-errCtx.Done():
 						return errCtx.Err()
-					case n, ok := <-ch:
-						if !ok {
-							return errCtx.Err()
-						}
-						if err := os.MkdirAll(filepath.Dir(n.fileName), 0o755); err != nil {
-							return fmt.Errorf("mkdir at %s: %w", filepath.Dir(n.fileName), err)
-						}
-						if err := os.WriteFile(n.fileName, n.value, 0o644); err != nil {
-							return fmt.Errorf("writing note at %s: %w", n.fileName, err)
-						}
-						cmd.logger.DebugContext(errCtx, "wrote note", "fileName", n.fileName)
-					}
-				}
-			})
-
-			notesWrote := 0
-			for _, commit := range commits {
-				var notes []string
-				for _, ref := range refs {
-					value, err := gitNotesShow(ctx, ref, commit)
-					if err != nil {
-						// This commit doesn't have this note ref, so we skip it.
-						continue
-					}
-					select {
-					case <-ctx.Done():
-						close(ch)
-						group.Wait()
-						return ctx.Err()
-					case ch <- noteInfo{
-						fileName: filepath.Join(output, commit, ref+".json"),
-
-						value: value,
+					case writeTasks <- exportPayload{
+						output: filepath.Join(output, "commits", commit, ref+".json"),
+						data:   note,
 					}:
-						notes = append(notes, ref)
-						notesWrote++
+					}
+
+					if manifest.Module == "" && note.Parsed.Pkg != "" {
+						manifest.Module = note.Parsed.Pkg
+					}
+
+					for _, bench := range note.Parsed.Benches {
+						bm, ok := benchmarks[bench.Name]
+						if !ok {
+							bm = &ManifestBenchmark{Name: bench.Name, Commits: []string{commit}}
+							benchmarks[bench.Name] = bm
+						} else {
+							if slices.Contains(bm.Commits, commit) {
+								continue
+							}
+							bm.Commits = append(bm.Commits, commit)
+						}
+						select {
+						case <-errCtx.Done():
+							return errCtx.Err()
+						case writeTasks <- exportPayload{
+							output: filepath.Join(output, "benchmarks", bench.Name, commit+".json"),
+							data:   bench,
+						}:
+						}
 					}
 				}
-				if len(notes) > 0 {
-					manifest.Commits = append(manifest.Commits, ManifestCommit{
-						Hash:     commit,
-						NoteRefs: notes,
-					})
+				if len(noteRefs) == 0 {
+					emptyCommits++
+					continue // No notes for this commit, skip it.
 				}
+				manifest.Commits = append(manifest.Commits, ManifestCommit{
+					Hash:     commit,
+					NoteRefs: noteRefs,
+				})
 			}
 
-			close(ch)
+			close(writeTasks)
 			if err := group.Wait(); err != nil {
 				return fmt.Errorf("exporting notes: %w", err)
 			}
 
-			manifestPath := filepath.Join(output, "manifest.json")
-			if err := os.MkdirAll(filepath.Dir(manifestPath), 0o755); err != nil {
-				return fmt.Errorf("mkdir at %s: %w", filepath.Dir(manifestPath), err)
-			}
-			manifestJSON, err := json.MarshalIndent(manifest, "", "  ")
-			if err != nil {
-				return fmt.Errorf("marshaling manifest: %w", err)
-			}
-			if err := os.WriteFile(manifestPath, manifestJSON, 0o644); err != nil {
-				return fmt.Errorf("writing manifest at %s: %w", manifestPath, err)
+			for _, bm := range benchmarks {
+				manifest.Benchmarks = append(manifest.Benchmarks, *bm)
 			}
 
-			commitsWithoutNotes := len(commits) - len(manifest.Commits)
-			cmd.logger.InfoContext(ctx, "export complete", "range", rangeSpec, "commits", len(commits), "notes", notesWrote, "commitsWithoutNotes", commitsWithoutNotes)
+			if err := writeManifest(manifest, output); err != nil {
+				return fmt.Errorf("writing manifest: %w", err)
+			}
+
+			cmd.logger.InfoContext(ctx, "export complete", "commits", len(manifest.Commits), "empty_commits", emptyCommits, "notes", len(manifest.Commits), "benchmarks", len(manifest.Benchmarks))
+
 			return nil
 		},
 	}
+}
+
+func exportNote(ctx context.Context, tasks <-chan exportPayload) error {
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case task, ok := <-tasks:
+			if !ok {
+				return ctx.Err()
+			}
+			if err := os.MkdirAll(filepath.Dir(task.output), 0o755); err != nil {
+				return fmt.Errorf("creating directory %s: %w", filepath.Dir(task.output), err)
+			}
+			data, err := json.Marshal(task.data)
+			if err != nil {
+				return fmt.Errorf("marshaling export data for %s: %w", task.output, err)
+			}
+			if err := os.WriteFile(task.output, data, 0o644); err != nil {
+				return fmt.Errorf("writing export file at %s: %w", task.output, err)
+			}
+		}
+	}
+}
+
+func writeManifest(manifest *Manifest, output string) error {
+	manifestPath := filepath.Join(output, "manifest.json")
+	if err := os.MkdirAll(filepath.Dir(manifestPath), 0o755); err != nil {
+		return fmt.Errorf("mkdir at %s: %w", filepath.Dir(manifestPath), err)
+	}
+	manifestJSON, err := json.MarshalIndent(manifest, "", "  ")
+	if err != nil {
+		return fmt.Errorf("marshaling manifest: %w", err)
+	}
+	if err := os.WriteFile(manifestPath, manifestJSON, 0o644); err != nil {
+		return fmt.Errorf("writing manifest at %s: %w", manifestPath, err)
+	}
+	return nil
 }
