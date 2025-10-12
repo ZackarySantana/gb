4f2dbd0865fc299f0d5ad79f708be9e910704f40
ZackarySantana <64446617+ZackarySantana@users.noreply.github.com>
2025-10-12 18:39:08 -0400

    refactor: simplify theme switcher

--- site/src/components/ThemeSwitch.tsx
+++ site/src/components/ThemeSwitch.tsx
@@ -3,19 +3,16 @@ import Select from "./Select";
 
 export default function ThemeSwitcher(props: { class?: string }) {
     return (
-        <div class={`flex items-center gap-2 ${props.class}`}>
-            <Select
-                onInput={(e) =>
-                    setTheme((e.target as HTMLSelectElement).value as ThemeName)
-                }
-                value={
-                    document.documentElement.getAttribute("data-theme") || ""
-                }
-            >
-                {THEMES.map((t) => (
-                    <option value={t}>{t}</option>
-                ))}
-            </Select>
-        </div>
+        <Select
+            onInput={(e) =>
+                setTheme((e.target as HTMLSelectElement).value as ThemeName)
+            }
+            value={document.documentElement.getAttribute("data-theme") || ""}
+            class={props.class}
+        >
+            {THEMES.map((t) => (
+                <option value={t}>{t}</option>
+            ))}
+        </Select>
     );
 }
